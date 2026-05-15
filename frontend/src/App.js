4a197aa5669e336075fbb08227bee26be512ab9b
Chamil Dunidu <hdchamildunidu@gmail.com>
2026-05-15 13:28:46 +0530

    add refresh and editReview

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [refresh, setRefresh] = useState(false);
+  const [editReview, setEditReview] = useState(null);
+
   return (
       <div className="App">
         <h1>⭐ Reviews & Ratings</h1>
