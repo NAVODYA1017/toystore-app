13f819ea617ee8486764c485b9900331d7fa46b3
Chamil Dunidu <hdchamildunidu@gmail.com>
2026-05-15 13:30:01 +0530

    finish up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
+import ReviewList from './components/ReviewList';
+import ReviewForm from './components/ReviewForm';
 import './App.css';
 
 function App() {
   const [refresh, setRefresh] = useState(false);
   const [editReview, setEditReview] = useState(null);
 
+  const handleSave = () => {
+    setRefresh(!refresh);
+    setEditReview(null);
+  };
+
   return (
       <div className="App">
         <h1>⭐ Reviews & Ratings</h1>
+        <ReviewForm onSave={handleSave} editReview={editReview} />
+        <ReviewList key={refresh} onEdit={setEditReview} />
       </div>
   );
 }
