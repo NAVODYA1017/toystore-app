b4bf48fcc4cc772fb7f22f267d223923f7c8a390
Chamil Dunidu <hdchamildunidu@gmail.com>
2026-05-16 22:58:22 +0530

    improve comments and readability

--- backend/src/main/java/com/toystore/backend/repository/ReviewRepository.java
+++ backend/src/main/java/com/toystore/backend/repository/ReviewRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
+// Handles all database operations for reviews
 @Repository
 public interface ReviewRepository extends MongoRepository<Review, String> {
 
